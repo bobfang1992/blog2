91a4fb0dee6deeb2b04ec08a116202d72abc20df
bobfang1992 <bob.fang.london@gmail.com>
2023-03-26 23:28:31 +0100

    init

--- src/blog/HelloWorld.js
+++ src/blog/HelloWorld.js
@@ -3,8 +3,11 @@ export function Component(params) {
     return (
         <>
             <h1>Hello World</h1>
-            <p>Creating a new blog as the old one refused to compiled it anymore, I do plan to port some of the old articles to this new
-                blog when I have time.
+            <p>
+                Creating a new blog as the old one now refuse to compile it anymore.
+                I hope by using only simple combination of naive React and Notion I do not run into this
+                kind of problem again.
+                I do plan to port some of the old articles to this new blog when I have time.
             </p>
         </>
     )
