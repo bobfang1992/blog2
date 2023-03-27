957aec55cac9a84a6dd376e35eb7f629f925e028
bobfang1992 <bob.fang.london@gmail.com>
2023-03-27 01:09:46 +0100

    try notion

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { articles } from './ArticleData'
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+    auth: process.env.REACT_APP_NOTION_API_KEY,
+});
 
 function ArticleListing(article) {
     return (
@@ -11,6 +16,23 @@ function ArticleListing(article) {
 }
 
 function Home() {
+    const [items, setItems] = useState([]);
+    const databaseId = "c3561f23082f491fa4b502a83db609ea";
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await notion.databases.query({ database_id: databaseId });
+                const items = response.results.map((item) => item.title[0].plain_text);
+                setItems(items);
+            } catch (error) {
+                console.error("Error fetching Notion data:", error);
+            }
+        };
+
+        fetchData();
+    }, [databaseId]);
+    console.log(items);
     return (
         <>
             <h1>Blog Roll</h1>
