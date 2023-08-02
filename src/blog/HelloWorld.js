53ae3760373d39c73a2e0097eb7082e57f6b551a
bobfang1992 <bob.fang.london@gmail.com>
2023-08-02 08:55:31 +0100

    fix typo

--- src/blog/HelloWorld.js
+++ src/blog/HelloWorld.js
@@ -4,7 +4,7 @@ export function Component(params) {
         <>
             <h1>Hello World</h1>
             <p>
-                Creating a new blog as the old one now refuse to compile it anymore.
+                Creating a new blog as the old one now refuse to compile itself anymore.
                 I hope by using only simple combination of naive React and Notion I do not run into this
                 kind of problem again.
                 I do plan to port some of the old articles to this new blog when I have time.
