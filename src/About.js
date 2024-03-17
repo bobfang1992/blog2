36595a237c8d0d11b8839583221e453777be766f
Bob Fang <bob.fang.london@gmail.com>
2024-03-17 15:53:15 -0400

    small syntax fix

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <>
             <h2>Bob Fang's Personal Website</h2>
-            <p>I am a software engineer working in Meta London. I am currently working on C++ but had previous experience with Python and Golang.</p>
+            <p>I am a software engineer working in Meta London. My main language now is C++ but had previous experience with Python and Golang.</p>
             <p>My day to day job is to optimise the ranking system of Instagram, it is a lot of fun.</p>
         </>
     )
--- src/blog/HelloWorld.js
+++ src/blog/HelloWorld.js
@@ -4,9 +4,10 @@ export function Component(params) {
         <>
             <h1>Hello World</h1>
             <p>
-                Creating a new blog as the old one now refuse to compile itself anymore.
+                Creating a new blog as the old one now refuse to compile itself.
                 I hope by using only simple combination of naive React and Notion I do not run into this
                 kind of problem again.
+                <br />
                 I do plan to port some of the old articles to this new blog when I have time.
             </p>
         </>
